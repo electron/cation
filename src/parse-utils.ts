b9edd8b569fe5e837f19b08bee39316c61e49d55
Samuel Attard <sattard@slack-corp.com>
2019-02-21 13:33:49 +0000

    build: add prettier magic

--- src/parse-utils.ts
+++ src/parse-utils.ts
@@ -3,21 +3,25 @@ const issueParser = require('github-issue-parser');
 
 const ISSUE_TEMPLATE_BASE_PATH = '.github/ISSUE_TEMPLATE/';
 
-export async function getIssueTemplates (context: Context) {
-  const {data: templatesList} = await context.github.repos.getContents(
+export async function getIssueTemplates(context: Context) {
+  const { data: templatesList } = await context.github.repos.getContents(
     context.repo({
-      path: ISSUE_TEMPLATE_BASE_PATH
-    })
-  )
+      path: ISSUE_TEMPLATE_BASE_PATH,
+    }),
+  );
 
-  if (!templatesList || !Array.isArray(templatesList)) return []
+  if (!templatesList || !Array.isArray(templatesList)) return [];
 
-  return Promise.all(templatesList.map(async template => {
-    const {data: {content}} = await context.github.repos.getContents(
-      context.repo({
-        path: ISSUE_TEMPLATE_BASE_PATH + template.name
-      })
-    )
-    return Buffer.from(content, 'base64').toString()
-  }))
+  return Promise.all(
+    templatesList.map(async template => {
+      const {
+        data: { content },
+      } = await context.github.repos.getContents(
+        context.repo({
+          path: ISSUE_TEMPLATE_BASE_PATH + template.name,
+        }),
+      );
+      return Buffer.from(content, 'base64').toString();
+    }),
+  );
 }
