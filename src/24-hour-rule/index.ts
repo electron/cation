3b417e8b5dafdedb12a71f49ad897e348b1052e9
Samuel Attard <samuel.r.attard@gmail.com>
2019-02-26 07:26:01 -0800

    refactor: consolidate new-pr detection and label add/removal

--- src/24-hour-rule/index.ts
+++ src/24-hour-rule/index.ts
@@ -1,16 +1,59 @@
 import { Application } from 'probot';
 
-import { NEW_PR_LABEL, MINIMUM_OPEN_TIME, EXCLUDE_LABELS, EXCLUDE_PREFIXES } from '../constants';
+import {
+  NEW_PR_LABEL,
+  MINIMUM_OPEN_TIME,
+  EXCLUDE_LABELS,
+  EXCLUDE_PREFIXES,
+  BACKPORT_LABEL,
+} from '../constants';
+import { WebhookPayloadWithRepository, Context } from 'probot/lib/context';
 
-const CHECK_INTERVAL = 1000 * 60;
+const CHECK_INTERVAL = 1000 * 60 * 2;
 
 export function setUp24HourRule(probot: Application) {
-  probot.on(['pull_request.opened', 'pull_request.unlabeled'], async context => {
-    const pr = context.payload.pull_request;
-
+  const shouldPRHaveLabel = (pr: WebhookPayloadWithRepository['pull_request']): boolean => {
     const prefix = pr.title.split(':')[0];
     const hasExcludedLabel = pr.labels.some((l: any) => EXCLUDE_LABELS.includes(l.name));
-    if (EXCLUDE_PREFIXES.includes(prefix) || hasExcludedLabel) return;
+    if (EXCLUDE_PREFIXES.includes(prefix) || hasExcludedLabel) return false;
+
+    const created = new Date(pr.created_at).getTime();
+    const now = Date.now();
+    return now - created < MINIMUM_OPEN_TIME;
+  };
+
+  const applyLabelToPR = async (
+    github: Context['github'],
+    pr: WebhookPayloadWithRepository['pull_request'],
+    repoOwner: string,
+    repoName: string,
+    shouldHaveLabel: boolean,
+  ) => {
+    if (shouldHaveLabel) {
+      probot.log('Found PR:', `${repoOwner}/${repoName}#${pr.number}`, 'should add label.');
+      await github.issues.addLabels({
+        number: pr.number,
+        labels: [NEW_PR_LABEL],
+        repo: repoName,
+        owner: repoOwner,
+      });
+    } else {
+      // Only attempt to remove if the label is actually on the PR
+      if (pr.labels.some((l: any) => l.name === BACKPORT_LABEL)) {
+        probot.log('Found PR:', `${repoOwner}/${repoName}#${pr.number}`, 'should remove label.');
+
+        await github.issues.removeLabel({
+          owner: repoOwner,
+          repo: repoName,
+          number: pr.number,
+          name: NEW_PR_LABEL,
+        });
+      }
+    }
+  };
+
+  probot.on(['pull_request.opened', 'pull_request.unlabeled'], async context => {
+    const pr = context.payload.pull_request;
 
     probot.log(
       'received PR:',
@@ -18,68 +61,48 @@ export function setUp24HourRule(probot: Application) {
       'checking now',
     );
 
-    const created = new Date(pr.created_at).getTime();
-    const now = Date.now();
-    const shouldAdd = now - created < MINIMUM_OPEN_TIME;
-
-    if (shouldAdd) {
-      context.github.issues.addLabels(
-        context.repo({
-          number: context.payload.pull_request.number,
-          labels: [NEW_PR_LABEL],
-        }),
-      );
-    }
+    await applyLabelToPR(
+      context.github,
+      pr,
+      context.repo({}).owner,
+      context.repo({}).repo,
+      shouldPRHaveLabel(pr),
+    );
   });
 
   runInterval();
-
-  setInterval(runInterval, CHECK_INTERVAL);
 
   async function runInterval() {
     probot.log('Running 24 hour rule check');
     const github = await probot.auth();
     const { data: installs } = await github.apps.listInstallations({});
     for (const install of installs) {
-      await runCron(probot, install.id);
+      try {
+        await runCron(probot, install.id);
+      } catch (err) {
+        probot.log('Failed to run cron for install:', install.id, err);
+      }
     }
+
+    setTimeout(runInterval, CHECK_INTERVAL);
   }
-}
 
-async function runCron(probot: Application, installId: number) {
-  const github = await probot.auth(installId);
-  const repos = await github.apps.listRepos({});
+  async function runCron(probot: Application, installId: number) {
+    const github = await probot.auth(installId);
+    const repos = await github.apps.listRepos({});
 
-  for (const repo of repos.data.repositories) {
-    probot.log('Running 24 hour cron job on repo:', `${repo.owner.login}/${repo.name}`);
-    // TODO: Paginate the PR list
-    const prs = await github.pullRequests.list({
-      owner: repo.owner.login,
-      repo: repo.name,
-      per_page: 100,
-      state: 'open',
-    });
+    for (const repo of repos.data.repositories) {
+      probot.log('Running 24 hour cron job on repo:', `${repo.owner.login}/${repo.name}`);
+      // TODO: Paginate the PR list
+      const prs = await github.pullRequests.list({
+        owner: repo.owner.login,
+        repo: repo.name,
+        per_page: 100,
+        state: 'open',
+      });
 
-    for (const pr of prs.data) {
-      if (!pr.labels.some(l => l.name === NEW_PR_LABEL)) continue;
-      const created = new Date(pr.created_at).getTime();
-      const now = Date.now();
-      const shouldRemove = now - created >= MINIMUM_OPEN_TIME;
-
-      if (shouldRemove) {
-        probot.log(
-          'Found PR:',
-          `${repo.owner.login}/${repo.name}#${pr.number}`,
-          `created ${now - created} ms ago`,
-          'therefore removing label.',
-        );
-
-        await github.issues.removeLabel({
-          owner: repo.owner.login,
-          repo: repo.name,
-          number: pr.number,
-          name: NEW_PR_LABEL,
-        });
+      for (const pr of prs.data) {
+        await applyLabelToPR(github, pr, repo.owner.login, repo.name, shouldPRHaveLabel(pr));
       }
     }
   }
