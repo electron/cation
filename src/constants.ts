91c5f31adfd14de7bec38c01ee444d5a9aa7e2b9
Samuel Attard <sam@electronjs.org>
2020-12-02 11:23:20 -0800

    feat: change 24 rule to 7 day rule for semver/minor and semver/major (#21)

--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,20 @@
 // 24 Hour Minimum Time
-export const MINIMUM_OPEN_TIME = 1000 * 60 * 60 * 24;
+export const MINIMUM_PATCH_OPEN_TIME = 1000 * 60 * 60 * 24;
+// 168 Hour Minimum Time
+export const MINIMUM_MINOR_OPEN_TIME = 1000 * 60 * 60 * 24 * 7;
+// 168 Hour Minimum Time
+export const MINIMUM_MAJOR_OPEN_TIME = 1000 * 60 * 60 * 24 * 7;
 
 // backport type labels
 export const NEW_PR_LABEL = 'new-pr 🌱';
 export const BACKPORT_LABEL = 'backport';
 export const BACKPORT_SKIP_LABEL = 'backport-check-skip';
 export const FAST_TRACK_LABEL = 'fast-track 🚅';
+export const SEMVER_LABELS = {
+  PATCH: 'semver/patch',
+  MINOR: 'semver/minor',
+  MAJOR: 'semver/major',
+};
 
 // exclusion labels
 export const EXCLUDE_LABELS = [BACKPORT_LABEL, BACKPORT_SKIP_LABEL, FAST_TRACK_LABEL];
