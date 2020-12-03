ad3a7f08ecb94e9c62bcbabed2aac711fbcfa907
Shelley Vohr <shelley.vohr@gmail.com>
2020-12-02 19:44:35 -0800

    feat: remove api-review/* labels added by humans

--- src/api-review-state.ts
+++ src/api-review-state.ts
@@ -1,14 +1,20 @@
 import { Application } from 'probot';
-import { API_REVIEW_CHECK_NAME, CheckRunStatus, REVIEW_LABELS, SEMVER_LABELS } from './constants';
+import { API_REVIEW_CHECK_NAME, REVIEW_LABELS, SEMVER_LABELS } from './constants';
+import { CheckRunStatus } from './enums';
 import { isAPIReviewRequired } from './utils/check-utils';
+import { getEnvVar } from './utils/env-util';
 
 export function setupAPIReviewStateManagement(probot: Application) {
   probot.on('pull_request.labeled', async context => {
     const { label, pull_request: pr } = context.payload;
 
+    if (!label) {
+      throw new Error('Something went wrong - label does not exist.');
+    }
+
     // If a PR is semver-minor or semver-major, automatically
     // add the 'api-review/requested 🗳' label.
-    if ([SEMVER_LABELS.MINOR, SEMVER_LABELS.MAJOR].includes(label!.name)) {
+    if ([SEMVER_LABELS.MINOR, SEMVER_LABELS.MAJOR].includes(label.name)) {
       probot.log(
         'Received a semver-minor or semver-major PR:',
         `${context.payload.repository.full_name}#${pr.number}`,
@@ -21,6 +27,20 @@ export function setupAPIReviewStateManagement(probot: Application) {
           labels: [REVIEW_LABELS.REQUESTED],
         }),
       );
+    } else if (Object.values(REVIEW_LABELS).includes(label.name)) {
+      const sender = context.payload.sender.login;
+
+      // Humans can only add the 'api-review/requested 🗳' manually.
+      if (sender !== getEnvVar('BOT_USER_NAME') && label.name !== REVIEW_LABELS.REQUESTED) {
+        probot.log(`${sender} tried to add ${label.name} - this is not permitted.`);
+        // Remove the label. Bad human.
+        context.github.issues.removeLabel(
+          context.repo({
+            issue_number: pr.number,
+            name: label.name,
+          }),
+        );
+      }
     }
   });
 
@@ -33,7 +53,7 @@ export function setupAPIReviewStateManagement(probot: Application) {
 
     // We want to prevent tampering with api-review/* labels other than
     // request labels - the bot should control the full review lifecycle.
-    if (Object.keys(REVIEW_LABELS).includes(label.name)) {
+    if (Object.values(REVIEW_LABELS).includes(label.name)) {
       const { data: allChecks } = await context.github.checks.listForRef(
         context.repo({
           ref: pr.head.sha,
@@ -55,6 +75,9 @@ export function setupAPIReviewStateManagement(probot: Application) {
           }),
         );
       } else {
+        const sender = context.payload.sender.login;
+        probot.log(`${sender} tried to remove ${label.name} - this is not permitted.`);
+
         // Put the label back. Bad human.
         context.github.issues.addLabels(
           context.repo({
---
+++ src/enums.ts
@@ -0,0 +1,12 @@
+export enum CheckRunStatus {
+  NEUTRAL = 'neutral',
+  FAILURE = 'failure',
+  SUCCESS = 'success',
+}
+
+export enum LogLevel {
+  LOG,
+  INFO,
+  WARN,
+  ERROR,
+}
