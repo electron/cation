60aa269edbe25922b41ccda38d00301e3a0ede75
Samuel Attard <sam@electronjs.org>
2020-12-10 10:47:21 -0800

    feat: add sentry error monitoring (#54)

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,30 @@
+import Sentry from '@sentry/node';
+
+if (process.env.SENTRY_DSN) {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+  });
+}
+
 import { Probot } from 'probot';
 import { setUp24HourRule } from './24-hour-rule';
 import { setupSemverLabelEnforcement } from './enforce-semver-labels';
 import { setupAPIReviewStateManagement } from './api-review-state';
 
 const probotHandler = async ({ app }: { app: Probot }) => {
+  app.on('error', event => {
+    for (const error of event.errors) {
+      if (process.env.SENTRY_DSN) {
+        Sentry.captureException(error, {
+          req: error.request,
+          extra: {
+            event: error.event,
+            status: error.status,
+          },
+        } as any);
+      }
+    }
+  });
   setUp24HourRule(app);
   setupSemverLabelEnforcement(app);
   setupAPIReviewStateManagement(app);
