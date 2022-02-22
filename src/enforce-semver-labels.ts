33afb3ce1dfa784f77e95c5e4e3729d7dbf8c41e
Shelley Vohr <shelley.vohr@gmail.com>
2022-02-22 19:17:54 +0100

    fix: pull_request.labeled handling

--- src/enforce-semver-labels.ts
+++ src/enforce-semver-labels.ts
@@ -9,56 +9,64 @@ const ALL_SEMVER_LABELS = [
 ];
 
 export function setupSemverLabelEnforcement(probot: Probot) {
-  probot.on(['pull_request.opened', 'pull_request.edited'], async context => {
-    const { pull_request: pr, repository } = context.payload;
+  probot.on(
+    [
+      'pull_request.opened',
+      'pull_request.unlabeled',
+      'pull_request.labeled',
+      'pull_request.synchronize',
+    ],
+    async context => {
+      const { pull_request: pr, repository } = context.payload;
 
-    probot.log(
-      'semver-enforce received PR:',
-      `${repository.full_name}#${pr.number}`,
-      'checking now',
-    );
+      probot.log(
+        'semver-enforce received PR:',
+        `${repository.full_name}#${pr.number}`,
+        'checking now',
+      );
 
-    const semverLabels = pr.labels.filter((l: any) => ALL_SEMVER_LABELS.includes(l.name));
-    if (semverLabels.length === 0) {
-      // Pending check -- not enough
-      await context.octokit.checks.create(
-        context.repo({
-          name: 'Semver Label Enforcement',
-          head_sha: pr.head.sha,
-          status: 'in_progress',
-          output: {
-            title: 'No semver/* label found',
-            summary: "We couldn't find a semver/* label, please add one",
-          },
-        }),
-      );
-    } else if (semverLabels.length > 1) {
-      // Pending check -- too many
-      await context.octokit.checks.create(
-        context.repo({
-          name: 'Semver Label Enforcement',
-          head_sha: pr.head.sha,
-          status: 'in_progress',
-          output: {
-            title: 'Multiple semver/* labels found',
-            summary: 'We found multiple semver/* labels, please remove one',
-          },
-        }),
-      );
-    } else {
-      // Pass check
-      await context.octokit.checks.create(
-        context.repo({
-          name: 'Semver Label Enforcement',
-          head_sha: pr.head.sha,
-          status: 'completed',
-          conclusion: 'success',
-          output: {
-            title: `Found "${semverLabels[0].name}"`,
-            summary: 'Found a single semver/* label, looking good here.',
-          },
-        }),
-      );
-    }
-  });
+      const semverLabels = pr.labels.filter((l: any) => ALL_SEMVER_LABELS.includes(l.name));
+      if (semverLabels.length === 0) {
+        // Pending check -- not enough
+        await context.octokit.checks.create(
+          context.repo({
+            name: 'Semver Label Enforcement',
+            head_sha: pr.head.sha,
+            status: 'in_progress',
+            output: {
+              title: 'No semver/* label found',
+              summary: "We couldn't find a semver/* label, please add one",
+            },
+          }),
+        );
+      } else if (semverLabels.length > 1) {
+        // Pending check -- too many
+        await context.octokit.checks.create(
+          context.repo({
+            name: 'Semver Label Enforcement',
+            head_sha: pr.head.sha,
+            status: 'in_progress',
+            output: {
+              title: 'Multiple semver/* labels found',
+              summary: 'We found multiple semver/* labels, please remove one',
+            },
+          }),
+        );
+      } else {
+        // Pass check
+        await context.octokit.checks.create(
+          context.repo({
+            name: 'Semver Label Enforcement',
+            head_sha: pr.head.sha,
+            status: 'completed',
+            conclusion: 'success',
+            output: {
+              title: `Found "${semverLabels[0].name}"`,
+              summary: 'Found a single semver/* label, looking good here.',
+            },
+          }),
+        );
+      }
+    },
+  );
 }
