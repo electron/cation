c73d4042aa60ca5772bf4873e5ffc881d54311bf
Shelley Vohr <shelley.vohr@gmail.com>
2022-11-08 23:48:36 +0100

    chore: fix fork PRs not updating label

--- src/api-review-state.ts
+++ src/api-review-state.ts
@@ -52,10 +52,7 @@ export const getPRReadyDate = (pr: PullRequest) => {
 };
 
 export async function addOrUpdateAPIReviewCheck(octokit: Context['octokit'], pr: PullRequest) {
-  log('addOrUpdateAPIReviewCheck', LogLevel.INFO, `Validating ${pr.number}`);
-
-  const owner = pr.head.repo.owner.login;
-  const repo = pr.head.repo.name;
+  log('addOrUpdateAPIReviewCheck', LogLevel.INFO, `Validating ${pr.number} by ${pr.user.login}`);
 
   type ListReviewsItem = GetResponseDataTypeFromEndpointMethod<typeof octokit.pulls.listReviews>[0];
 
@@ -65,15 +62,29 @@ export async function addOrUpdateAPIReviewCheck(octokit: Context['octokit'], pr:
 
   type CommentOrReview = ListReviewsItem | ListCommentsItem;
 
+  const fork = pr.head.repo.fork;
+  const owner = pr.base.repo.owner.login;
+  const repo = pr.head.repo.name;
+
+  if (fork) {
+    log(
+      'addOrUpdateAPIReviewCheck',
+      LogLevel.INFO,
+      `${pr.number} is a fork - checks will not be created or updated`,
+    );
+  }
+
   // Fetch the latest API Review check for the PR.
-  const checkRun = (
-    await octokit.checks.listForRef({
-      ref: pr.head.sha,
-      per_page: 100,
-      owner,
-      repo,
-    })
-  ).data.check_runs.find((run) => run.name === API_REVIEW_CHECK_NAME);
+  const checkRun = fork
+    ? null
+    : (
+        await octokit.checks.listForRef({
+          ref: pr.head.sha,
+          per_page: 100,
+          owner,
+          repo,
+        })
+      ).data.check_runs.find((run) => run.name === API_REVIEW_CHECK_NAME);
 
   const resetToNeutral = async () => {
     if (!checkRun) return;
@@ -201,6 +212,9 @@ export async function addOrUpdateAPIReviewCheck(octokit: Context['octokit'], pr:
   );
 
   const users = { approved, declined, requestedChanges };
+
+  // If the PR is a fork PR, return early as the Checks API doesn't work.
+  if (fork) return users;
 
   // Update the GitHub Check with appropriate API review information.
   const updateCheck = async (
