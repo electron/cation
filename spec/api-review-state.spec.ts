a22b8e71ea68a9d3d31c0734dce1d3c663c8a77a
Shelley Vohr <shelley.vohr@gmail.com>
2022-11-09 14:48:49 +0100

    test: add regression tests for API review state

--- spec/api-review-state.spec.ts
+++ spec/api-review-state.spec.ts
@@ -16,9 +16,22 @@ import {
   MINIMUM_PATCH_OPEN_TIME,
   API_REVIEW_CHECK_NAME,
   NEW_PR_LABEL,
+  API_WORKING_GROUP,
 } from '../src/constants';
 
 import { CheckRunStatus } from '../src/enums';
+
+const API_WORKING_GROUP_MEMBERS = [
+  { login: 'ckerr' },
+  { login: 'codebytere' },
+  { login: 'itsananderson' },
+  { login: 'jkleinsc' },
+  { login: 'marshallofsound' },
+  { login: 'miniak' },
+  { login: 'nornagon' },
+  { login: 'samuelmaddock' },
+  { login: 'zcbenz' },
+];
 
 const handler = async (app: Probot) => {
   setupAPIReviewStateManagement(app);
@@ -188,7 +201,7 @@ describe('api review', () => {
     expect(response3).toEqual(undefined);
   });
 
-  it(`correctly update api review check when no ${REVIEW_LABELS} found`, async () => {
+  it(`correctly updates api review check when no review labels are found`, async () => {
     const payload = require('./fixtures/api-review-state/pull_request.no_review_label.json');
 
     nock('https://api.github.com')
@@ -225,34 +238,437 @@ describe('api review', () => {
     });
   });
 
-  it('correctly update api review lables when pr review is submitted', async () => {
-    const payload = require('./fixtures/api-review-state/pull_request_review.submitted.json');
+  describe('correctly updates API Review data when a PR review is submitted', () => {
+    describe('from the base repo', () => {
+      it('updates the check when there is one API LGTM', async () => {
+        const payload = require('./fixtures/api-review-state/pull_request_review/base/submitted.json');
+        const { pull_request } = payload;
 
-    nock('https://api.github.com')
-      .get(
-        `/repos/electron/electron/commits/${payload.pull_request.head.sha}/check-runs?per_page=100`,
-      )
-      .reply(200, { check_runs: [] });
+        nock('https://api.github.com')
+          .get(`/orgs/electron/teams/${API_WORKING_GROUP}/members`)
+          .reply(200, API_WORKING_GROUP_MEMBERS);
 
-    nock('https://api.github.com')
-      .get(`/repos/electron/electron/issues/${payload.number}/labels?per_page=100&page=1`)
-      .reply(200, []);
+        nock('https://api.github.com')
+          .get(`/repos/electron/electron/commits/${pull_request.head.sha}/check-runs?per_page=100`)
+          .reply(200, {
+            check_runs: [
+              {
+                name: API_REVIEW_CHECK_NAME,
+                id: '12345',
+              },
+            ],
+          });
 
-    nock('https://api.github.com')
-      .post(`/repos/electron/electron/issues/${payload.number}/labels`, (body) => {
-        expect(body).toEqual([REVIEW_LABELS.REQUESTED]);
-        return true;
-      })
-      .reply(200);
+        const r1 = require('./fixtures/api-review-state/pull_request_review/base/review_lgtm.json');
+        nock('https://api.github.com')
+          .get(`/repos/electron/electron/pulls/${pull_request.number}/reviews`)
+          .reply(200, [r1]);
 
-    await robot.receive({
-      id: '123-456',
-      name: 'pull_request_review',
-      payload,
+        const c1 = require('./fixtures/api-review-state/pull_request_review/base/comment_neutral.json');
+        nock('https://api.github.com')
+          .get(`/repos/electron/electron/issues/${pull_request.number}/comments`)
+          .reply(200, [c1]);
+
+        nock('https://api.github.com')
+          .get(`/repos/electron/electron/issues/${pull_request.number}/labels?per_page=100&page=1`)
+          .reply(200, [
+            {
+              id: 208045946,
+              node_id: 'MDU6TGFiZWwyMDgwNDU5NDY=',
+              url: `https://api.github.com/repos/electron/electron/labels/${REVIEW_LABELS.REQUESTED}`,
+              name: REVIEW_LABELS.REQUESTED,
+              description: '',
+              color: 'f29513',
+              default: true,
+            },
+          ]);
+
+        nock('https://api.github.com')
+          .post('/repos/electron/electron/check-runs', (body) => {
+            expect(body).toMatchObject({
+              name: API_REVIEW_CHECK_NAME,
+              status: 'in_progress',
+              output: {
+                summary: `#### Approved
+
+* @codebytere
+`,
+                title: 'Pending (1/2 LGTMs - ready on 2022-11-08)',
+              },
+            });
+            return true;
+          })
+          .reply(200);
+
+        await robot.receive({
+          id: '123-456',
+          name: 'pull_request_review',
+          payload,
+        });
+      });
+
+      it('sets the label to APPROVED when there are two API LGTMs and updates the check', async () => {
+        const payload = require('./fixtures/api-review-state/pull_request_review/base/submitted.json');
+        const { pull_request } = payload;
+
+        nock('https://api.github.com')
+          .get(`/orgs/electron/teams/${API_WORKING_GROUP}/members`)
+          .reply(200, API_WORKING_GROUP_MEMBERS);
+
+        nock('https://api.github.com')
+          .get(`/repos/electron/electron/commits/${pull_request.head.sha}/check-runs?per_page=100`)
+          .reply(200, {
+            check_runs: [
+              {
+                name: API_REVIEW_CHECK_NAME,
+                id: '12345',
+              },
+            ],
+          });
+
+        const r1 = require('./fixtures/api-review-state/pull_request_review/base/review_lgtm.json');
+        const r2 = require('./fixtures/api-review-state/pull_request_review/base/review_lgtm_2.json');
+        nock('https://api.github.com')
+          .get(`/repos/electron/electron/pulls/${pull_request.number}/reviews`)
+          .reply(200, [r1, r2]);
+
+        nock('https://api.github.com')
+          .get(`/repos/electron/electron/issues/${pull_request.number}/comments`)
+          .reply(200, []);
+
+        nock('https://api.github.com')
+          .get(`/repos/electron/electron/issues/${pull_request.number}/labels?per_page=100&page=1`)
+          .reply(200, [
+            {
+              id: 208045946,
+              node_id: 'MDU6TGFiZWwyMDgwNDU5NDY=',
+              url: `https://api.github.com/repos/electron/electron/labels/${REVIEW_LABELS.REQUESTED}`,
+              name: REVIEW_LABELS.REQUESTED,
+              description: '',
+              color: 'f29513',
+              default: true,
+            },
+          ]);
+
+        nock('https://api.github.com')
+          .post('/repos/electron/electron/check-runs', (body) => {
+            expect(body).toMatchObject({
+              name: 'API Review',
+              head_sha: '7562c752c4352aff581c9b60826de3a3c369f564',
+              status: 'in_progress',
+              output: {
+                summary: `#### Approved
+
+* @codebytere
+* @nornagon
+`,
+                title: 'Pending (2/2 LGTMs - ready on 2022-11-08)',
+              },
+            });
+            return true;
+          })
+          .reply(200);
+
+        const encoded = encodeURIComponent(REVIEW_LABELS.REQUESTED);
+        nock('https://api.github.com')
+          .delete(`/repos/electron/electron/issues/${pull_request.number}/labels/${encoded}`)
+          .reply(200, [
+            {
+              id: 208045946,
+              node_id: 'MDU6TGFiZWwyMDgwNDU5NDY=',
+              url: `https://api.github.com/repos/electron/electron/labels/${encoded}`,
+              name: REVIEW_LABELS.REQUESTED,
+              description: '',
+              color: 'f29513',
+              default: true,
+            },
+          ]);
+
+        nock('https://api.github.com')
+          .get(`/repos/electron/electron/issues/${pull_request.number}/labels?per_page=100&page=1`)
+          .reply(200, []);
+
+        nock('https://api.github.com')
+          .post(`/repos/electron/electron/issues/${pull_request.number}/labels`, (body) => {
+            expect(body).toEqual([REVIEW_LABELS.APPROVED]);
+            return true;
+          })
+          .reply(200);
+
+        await robot.receive({
+          id: '123-456',
+          name: 'pull_request_review',
+          payload,
+        });
+      });
+
+      it('sets the label to DECLINED when there is one API DECLINED', async () => {
+        const payload = require('./fixtures/api-review-state/pull_request_review/base/submitted.json');
+        const { pull_request } = payload;
+
+        nock('https://api.github.com')
+          .get(`/orgs/electron/teams/${API_WORKING_GROUP}/members`)
+          .reply(200, API_WORKING_GROUP_MEMBERS);
+
+        nock('https://api.github.com')
+          .get(`/repos/electron/electron/commits/${pull_request.head.sha}/check-runs?per_page=100`)
+          .reply(200, {
+            check_runs: [
+              {
+                name: API_REVIEW_CHECK_NAME,
+                id: '12345',
+              },
+            ],
+          });
+
+        const r1 = require('./fixtures/api-review-state/pull_request_review/base/review_declined.json');
+        nock('https://api.github.com')
+          .get(`/repos/electron/electron/pulls/${pull_request.number}/reviews`)
+          .reply(200, [r1]);
+
+        nock('https://api.github.com')
+          .get(`/repos/electron/electron/issues/${pull_request.number}/comments`)
+          .reply(200, []);
+
+        nock('https://api.github.com')
+          .get(`/repos/electron/electron/issues/${pull_request.number}/labels?per_page=100&page=1`)
+          .reply(200, [
+            {
+              id: 208045946,
+              node_id: 'MDU6TGFiZWwyMDgwNDU5NDY=',
+              url: `https://api.github.com/repos/electron/electron/labels/${REVIEW_LABELS.REQUESTED}`,
+              name: REVIEW_LABELS.REQUESTED,
+              description: '',
+              color: 'f29513',
+              default: true,
+            },
+          ]);
+
+        nock('https://api.github.com')
+          .post('/repos/electron/electron/check-runs', (body) => {
+            expect(body).toMatchObject({
+              name: 'API Review',
+              head_sha: '7562c752c4352aff581c9b60826de3a3c369f564',
+              status: 'in_progress',
+              output: {
+                summary: `#### Declined
+
+* @jkleinsc
+`,
+                title: 'Pending (0/2 LGTMs - ready on 2022-11-08)',
+              },
+            });
+            return true;
+          })
+          .reply(200);
+
+        const encoded = encodeURIComponent(REVIEW_LABELS.REQUESTED);
+        nock('https://api.github.com')
+          .delete(`/repos/electron/electron/issues/${pull_request.number}/labels/${encoded}`)
+          .reply(200, [
+            {
+              id: 208045946,
+              node_id: 'MDU6TGFiZWwyMDgwNDU5NDY=',
+              url: `https://api.github.com/repos/electron/electron/labels/${encoded}`,
+              name: REVIEW_LABELS.REQUESTED,
+              description: '',
+              color: 'f29513',
+              default: true,
+            },
+          ]);
+
+        nock('https://api.github.com')
+          .get(`/repos/electron/electron/issues/${pull_request.number}/labels?per_page=100&page=1`)
+          .reply(200, []);
+
+        nock('https://api.github.com')
+          .post(`/repos/electron/electron/issues/${pull_request.number}/labels`, (body) => {
+            expect(body).toEqual([REVIEW_LABELS.DECLINED]);
+            return true;
+          })
+          .reply(200);
+
+        await robot.receive({
+          id: '123-456',
+          name: 'pull_request_review',
+          payload,
+        });
+      });
+    });
+
+    describe('from a fork repo', () => {
+      it('the label remains REQUESTED when there is one API LGTM', async () => {
+        const payload = require('./fixtures/api-review-state/pull_request_review/fork/submitted.json');
+        const { pull_request } = payload;
+
+        nock('https://api.github.com')
+          .get(`/orgs/electron/teams/${API_WORKING_GROUP}/members`)
+          .reply(200, API_WORKING_GROUP_MEMBERS);
+
+        const r1 = require('./fixtures/api-review-state/pull_request_review/fork/review_lgtm.json');
+        nock('https://api.github.com')
+          .get(`/repos/electron/electron/pulls/${pull_request.number}/reviews`)
+          .reply(200, [r1]);
+
+        const c1 = require('./fixtures/api-review-state/pull_request_review/fork/comment_neutral.json');
+        nock('https://api.github.com')
+          .get(`/repos/electron/electron/issues/${pull_request.number}/comments`)
+          .reply(200, [c1]);
+
+        nock('https://api.github.com')
+          .get(`/repos/electron/electron/issues/${pull_request.number}/labels?per_page=100&page=1`)
+          .reply(200, [
+            {
+              id: 208045946,
+              node_id: 'MDU6TGFiZWwyMDgwNDU5NDY=',
+              url: `https://api.github.com/repos/electron/electron/labels/${REVIEW_LABELS.REQUESTED}`,
+              name: REVIEW_LABELS.REQUESTED,
+              description: '',
+              color: 'f29513',
+              default: true,
+            },
+          ]);
+
+        await robot.receive({
+          id: '123-456',
+          name: 'pull_request_review',
+          payload,
+        });
+      });
+
+      it('sets the label to APPROVED when there are two API LGTMs', async () => {
+        const payload = require('./fixtures/api-review-state/pull_request_review/fork/submitted.json');
+        const { pull_request } = payload;
+
+        nock('https://api.github.com')
+          .get(`/orgs/electron/teams/${API_WORKING_GROUP}/members`)
+          .reply(200, API_WORKING_GROUP_MEMBERS);
+
+        const r1 = require('./fixtures/api-review-state/pull_request_review/fork/review_lgtm.json');
+        const r2 = require('./fixtures/api-review-state/pull_request_review/fork/review_lgtm_2.json');
+        nock('https://api.github.com')
+          .get(`/repos/electron/electron/pulls/${pull_request.number}/reviews`)
+          .reply(200, [r1, r2]);
+
+        const c1 = require('./fixtures/api-review-state/pull_request_review/fork/comment_neutral.json');
+        nock('https://api.github.com')
+          .get(`/repos/electron/electron/issues/${pull_request.number}/comments`)
+          .reply(200, [c1]);
+
+        nock('https://api.github.com')
+          .get(`/repos/electron/electron/issues/${pull_request.number}/labels?per_page=100&page=1`)
+          .reply(200, [
+            {
+              id: 208045946,
+              node_id: 'MDU6TGFiZWwyMDgwNDU5NDY=',
+              url: `https://api.github.com/repos/electron/electron/labels/${REVIEW_LABELS.REQUESTED}`,
+              name: REVIEW_LABELS.REQUESTED,
+              description: '',
+              color: 'f29513',
+              default: true,
+            },
+          ]);
+
+        const encoded = encodeURIComponent(REVIEW_LABELS.REQUESTED);
+        nock('https://api.github.com')
+          .delete(`/repos/electron/electron/issues/${pull_request.number}/labels/${encoded}`)
+          .reply(200, [
+            {
+              id: 208045946,
+              node_id: 'MDU6TGFiZWwyMDgwNDU5NDY=',
+              url: `https://api.github.com/repos/electron/electron/labels/${encoded}`,
+              name: REVIEW_LABELS.REQUESTED,
+              description: '',
+              color: 'f29513',
+              default: true,
+            },
+          ]);
+
+        nock('https://api.github.com')
+          .get(`/repos/electron/electron/issues/${pull_request.number}/labels?per_page=100&page=1`)
+          .reply(200, []);
+
+        nock('https://api.github.com')
+          .post(`/repos/electron/electron/issues/${pull_request.number}/labels`, (body) => {
+            expect(body).toEqual([REVIEW_LABELS.APPROVED]);
+            return true;
+          })
+          .reply(200);
+
+        await robot.receive({
+          id: '123-456',
+          name: 'pull_request_review',
+          payload,
+        });
+      });
+
+      it('sets the label to DECLINED when there is one API DECLINED', async () => {
+        const payload = require('./fixtures/api-review-state/pull_request_review/fork/submitted.json');
+        const { pull_request } = payload;
+
+        nock('https://api.github.com')
+          .get(`/orgs/electron/teams/${API_WORKING_GROUP}/members`)
+          .reply(200, API_WORKING_GROUP_MEMBERS);
+
+        const r1 = require('./fixtures/api-review-state/pull_request_review/fork/review_declined.json');
+        nock('https://api.github.com')
+          .get(`/repos/electron/electron/pulls/${pull_request.number}/reviews`)
+          .reply(200, [r1]);
+
+        nock('https://api.github.com')
+          .get(`/repos/electron/electron/issues/${pull_request.number}/comments`)
+          .reply(200, []);
+
+        nock('https://api.github.com')
+          .get(`/repos/electron/electron/issues/${pull_request.number}/labels?per_page=100&page=1`)
+          .reply(200, [
+            {
+              id: 208045946,
+              node_id: 'MDU6TGFiZWwyMDgwNDU5NDY=',
+              url: `https://api.github.com/repos/electron/electron/labels/${REVIEW_LABELS.REQUESTED}`,
+              name: REVIEW_LABELS.REQUESTED,
+              description: '',
+              color: 'f29513',
+              default: true,
+            },
+          ]);
+
+        const encoded = encodeURIComponent(REVIEW_LABELS.REQUESTED);
+        nock('https://api.github.com')
+          .delete(`/repos/electron/electron/issues/${pull_request.number}/labels/${encoded}`)
+          .reply(200, [
+            {
+              id: 208045946,
+              node_id: 'MDU6TGFiZWwyMDgwNDU5NDY=',
+              url: `https://api.github.com/repos/electron/electron/labels/${encoded}`,
+              name: REVIEW_LABELS.REQUESTED,
+              description: '',
+              color: 'f29513',
+              default: true,
+            },
+          ]);
+
+        nock('https://api.github.com')
+          .get(`/repos/electron/electron/issues/${pull_request.number}/labels?per_page=100&page=1`)
+          .reply(200, []);
+
+        nock('https://api.github.com')
+          .post(`/repos/electron/electron/issues/${pull_request.number}/labels`, (body) => {
+            expect(body).toEqual([REVIEW_LABELS.DECLINED]);
+            return true;
+          })
+          .reply(200);
+
+        await robot.receive({
+          id: '123-456',
+          name: 'pull_request_review',
+          payload,
+        });
+      });
     });
   });
 
-  it(`correctly adds ${REVIEW_LABELS.REQUESTED} label if pr contains semver/major or semver/minor lable and not contains exclusion labels`, async () => {
+  it(`adds ${REVIEW_LABELS.REQUESTED} label if pr has semver/major or semver/minor label and no exclusion labels`, async () => {
     const payload = require('./fixtures/api-review-state/pull_request.semver-minor.json');
 
     nock('https://api.github.com')
